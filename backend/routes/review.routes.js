22a73450e665a13590b06e282d8e4d83342526b1
Sampad Mondal <sampadm310@outlook.com>
2026-02-28 21:27:59 +0530

    Review delete routes added

--- backend/routes/review.routes.js
+++ backend/routes/review.routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { postReview } from '../controllers/review.controller.js'
+import { postReview, deleteReview } from '../controllers/review.controller.js'
 import verifyJWT from '../middleware/verifyJWT.middleware.js'
 
 const router = express.Router()
 
 router.post("/:itemId", verifyJWT, postReview ) // chunks route
+
+router.delete("/:reviewId", verifyJWT, deleteReview ) // chunks route
 
 export default router
