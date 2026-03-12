c7ca905ab693103b714a88f90d38ecfe41b1df7c
Sampad Mondal <sampadm310@outlook.com>
2026-03-12 15:01:00 +0530

    forgot password, verify token, reset password routes created

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {signup, login, logout, forgotPassword } from "../controllers/auth.controller.js";
+import {signup, login, logout, forgotPassword, validateToken, resetPassword } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -10,5 +10,9 @@ router.post("/login", login) // chunks route
 router.post("/logout", logout) // chunks route
 
 router.post("/forgot-password", forgotPassword) // chunks route
+
+router.get("/validate-token/:token", validateToken) // chunks route
+
+router.post("/reset-password", resetPassword) // chunks route
 
 export default router
