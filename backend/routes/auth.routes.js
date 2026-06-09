29d5a34efcbd85d93082bc0d01502e421c1c895d
Sampad Mondal <sampadm310@outlook.com>
2026-06-10 00:29:31 +0530

    rate limiting middleware added in login, sign up and forgot password routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {signup, login, logout, forgotPassword, validateToken, resetPassword, getUserDetailsByToken, updateUserDetailsByToken } from "../controllers/auth.controller.js";
 import verifyJWT from "../middleware/verifyJWT.middleware.js";
+import { authLimiter, forgotPasswordLimiter } from "../middleware/rateLimit.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup); // chunks route
+router.post("/signup", authLimiter, signup); // chunks route
 
-router.post("/login", login) // chunks route
+router.post("/login", authLimiter, login) // chunks route
 
 router.post("/logout", logout) // chunks route
 
-router.post("/forgot-password", forgotPassword) // chunks route
+router.post("/forgot-password", forgotPasswordLimiter, forgotPassword) // chunks route
 
 router.get("/validate-token/:token", validateToken) // chunks route
 
