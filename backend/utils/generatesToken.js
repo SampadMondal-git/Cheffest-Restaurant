4ca7073e26ce1cea7f8acbc0de72039f932bbe2e
Sampad Mondal <sampadm310@outlook.com>
2026-08-24 22:26:44 +0530

    generate token logic rebuild

--- backend/utils/generatesToken.js
+++ backend/utils/generatesToken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (user, res, rememberMe = false) => {
     const token = jwt.sign(
-        { userId: user._id, role: user.role },
+        { userId: user._id, role: user.role, position: user.position },
         process.env.JWT_SECRET_KEY,
         { expiresIn: rememberMe ? "30d" : "1d" }
     );
 
     const cookieOptions = {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
+        secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
         path: "/",
     };
