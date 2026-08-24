3846bf5f1b3cdf13a30e7352becede707c2d9c9a
Sampad Mondal <sampadm310@outlook.com>
2026-08-24 22:22:33 +0530

    verify token logic added

--- backend/middleware/verifyJWT.middleware.js
+++ backend/middleware/verifyJWT.middleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const verifyJWT = (req, res, next) => {
     try {
-        const token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
+        const token = req.headers.authorization?.split(" ")[1] || req.cookies.jwt;
 
         if (!token) {
             return res.status(401).json({ message: "Token missing" });
