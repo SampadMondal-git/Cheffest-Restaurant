d06389a8740284524d02da46a5a3e45b5d9dc085
Sampad Mondal <sampadm310@outlook.com>
2026-09-07 22:13:09 +0530

    Major bug fix

--- backend/routes/reservation.routes.js
+++ backend/routes/reservation.routes.js
@@ -6,7 +6,7 @@ import { bookReservation, getReservationByUserToken, fetchReservationById, updat
 
 const router = express.Router();
 
-router.post("/add-reservation", bookReservation); // Public booking; user is optional
+router.post("/add-reservation", verifyJWT, bookReservation);
 
 router.get("/get-reservation-by-user", verifyJWT, getReservationByUserToken); // chunks route
 
